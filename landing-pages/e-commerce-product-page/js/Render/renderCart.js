d0f981c9ef01a7f6514db406f233c3df71ca88ba
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2023-10-30 09:45:00 +0100

    fix

--- landing-pages/e-commerce-product-page/js/Render/renderCart.js
+++ landing-pages/e-commerce-product-page/js/Render/renderCart.js
@@ -1,51 +1,51 @@
-import { cartContent } from "./../index.js";
-import { products } from "./../Products/products.js";
+// import { cartContent } from "./../index.js";
+// import { products } from "./../Products/products.js";
 
-export function createCartHtml() {
-  cartContent.innerHTML = "";
+// export function createCartHtml() {
+//   cartContent.innerHTML = "";
 
-  const localItem = localStorage.getItem("item");
-  const localAmount = localStorage.getItem("amount");
-  const localPrice = localStorage.getItem("price");
+//   const localItem = localStorage.getItem("item");
+//   const localAmount = localStorage.getItem("amount");
+//   const localPrice = localStorage.getItem("price");
 
-  const element = document.createElement("div");
+//   const element = document.createElement("div");
 
-  const childContainer1 = document.createElement("div");
-  element.append(childContainer1);
+//   const childContainer1 = document.createElement("div");
+//   element.append(childContainer1);
 
-  const childContainer2 = document.querySelector("div");
-  element.append(childContainer2);
+//   const childContainer2 = document.querySelector("div");
+//   element.append(childContainer2);
 
-  const childContainer3 = document.createElement("div");
-  element.append(childContainer3);
+//   const childContainer3 = document.createElement("div");
+//   element.append(childContainer3);
 
-  const thumbnail = document.createElement("img");
-  thumbnail.src = `${products[0].thumbnail}`;
-  thumbnail.setAttribute("alt", `the shoes displayed elegantly`);
-  childContainer1.append(thumbnail);
+//   const thumbnail = document.createElement("img");
+//   thumbnail.src = `${products[0].thumbnail}`;
+//   thumbnail.setAttribute("alt", `the shoes displayed elegantly`);
+//   childContainer1.append(thumbnail);
 
-  const childTextContainer = document.createElement("div");
-  childContainer2.append(childTextContainer);
+//   const childTextContainer = document.createElement("div");
+//   childContainer2.append(childTextContainer);
 
-  const childPriceContainer = document.createElement("div");
-  childContainer2.append(childPriceContainer);
+//   const childPriceContainer = document.createElement("div");
+//   childContainer2.append(childPriceContainer);
 
-  const trash = document.createElement("img");
-  trash.src = "./images/icon-delete.svg";
-  trash.setAttribute("alt", "click to empty cart");
-  childContainer3.append(trash);
+//   const trash = document.createElement("img");
+//   trash.src = "./images/icon-delete.svg";
+//   trash.setAttribute("alt", "click to empty cart");
+//   childContainer3.append(trash);
 
-  const title = document.createElement("span");
-  title.textContent = `${localItem}`;
-  childTextContainer.append(title);
+//   const title = document.createElement("span");
+//   title.textContent = `${localItem}`;
+//   childTextContainer.append(title);
 
-  const price = document.createElement("span");
-  price.textContent = `$ ${localPrice} x ${localAmount}`;
-  childPriceContainer.append(price);
+//   const price = document.createElement("span");
+//   price.textContent = `$ ${localPrice} x ${localAmount}`;
+//   childPriceContainer.append(price);
 
-  const total = document.createElement("span");
-  total.textContent = `$ ${localPrice * localAmount}`;
-  childPriceContainer.append(total);
+//   const total = document.createElement("span");
+//   total.textContent = `$ ${localPrice * localAmount}`;
+//   childPriceContainer.append(total);
 
-  return element;
-}
+//   return element;
+// }
--- landing-pages/e-commerce-product-page/js/cart/updateCartContent.js
+++ landing-pages/e-commerce-product-page/js/cart/updateCartContent.js
@@ -1,12 +1,35 @@
 import { cartContent } from "./../index.js";
-import { createCartHtml } from "../Render/renderCart.js";
+import { products } from "../Products/products.js";
 
 export function updateCartContent() {
   const count = localStorage.getItem("amount");
 
+  const localItem = localStorage.getItem("item");
+  const localAmount = localStorage.getItem("amount");
+  const localPrice = localStorage.getItem("price");
+
   if (count !== "0") {
-    const cartHtml = createCartHtml();
-    cartContent.append(cartHtml);
+    cartContent.innerHTML = `
+  <div class="cart-container-wrap">
+    <div class="cart-container">
+      <div>
+        <img src="${products[0].thumbnail}" alt="shoes displayed elegantly">
+      </div>
+      <div class="text-wrap">
+        <span class="text-title">${localItem}</span>
+        <div>
+          <span class="text-price">$${localPrice}.00 x ${localAmount}</span>
+          <span>$${localPrice * localAmount}.00</span>
+        </div>
+      </div>
+      <div class="rem-ord">
+        <img src="./images/icon-delete.svg" alt="click to empty cart">
+      </div>
+    </div>
+    <div class="checkout-wrap">
+      <button class="cta checkout-btn">Checkout</button>
+    </div>
+  </div>`;
   } else {
     cartContent.innerHTML = "<span>Your cart is empty.</span>";
   }
